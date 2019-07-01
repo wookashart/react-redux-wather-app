284e456eee0118464db59e8abe59b7aada4b6a89
Łukasz Krawczyk <lukasz.krawczyk@vml.com>
2019-07-01 09:54:20 +0200

    Custom weather icons added

--- src/components/result/WeatherIcon.js
+++ src/components/result/WeatherIcon.js
@@ -4,13 +4,15 @@ import clearSkyDay from '../../images/iconfinder_weather-01_1530392.png';
 import clearSkyNight from '../../images/iconfinder_weather-10_1530382.png';
 import fewCloudsDay from '../../images/iconfinder_weather-02_1530391.png';
 import fewCloudsNight from '../../images/iconfinder_weather-11_1530383.png';
-import scatteredClouds from '../../images/iconfinder_weather-22_1530369.png';
+import clouds from '../../images/iconfinder_weather-22_1530369.png';
+import showerRain from '../../images/iconfinder_weather-32_1530362.png';
+import rainDay from '../../images/iconfinder_weather-03_1530390.png';
+import rainNight from '../../images/iconfinder_weather-14_1530379.png';
+import thunderstorm from '../../images/iconfinder_weather-23_1530363.png';
+import snow from '../../images/iconfinder_weather-24_1530371.png';
+import mist from '../../images/iconfinder_weather-27_1530368.png';
 
 class WeatherIcon extends Component {
-    state = {
-        icon: ''
-    }
-
     getWeatherIcon = description => {
         const now = new Date();
         const time = Math.round(now.getTime() / 1000);
@@ -20,22 +22,19 @@ class WeatherIcon extends Component {
             icon = time >= this.props.sunrise && time < this.props.sunset ? clearSkyDay : clearSkyNight;
         } else if (description === 'few clouds') {
             icon = time >= this.props.sunrise && time < this.props.sunset ? fewCloudsDay : fewCloudsNight;
-        } else if (description === 'scattered clouds') {
-            icon = scatteredClouds;
-        } else if (description === 'broken clouds') {
-
+        } else if (description === 'scattered clouds' || description === 'broken clouds') {
+            icon = clouds;
         } else if (description === 'shower rain') {
-
+            icon = showerRain;
         } else if (description === 'rain') {
-
+            icon = time >= this.props.sunrise && time < this.props.sunset ? rainDay : rainNight;
         } else if (description === 'thunderstorm') {
-
+            icon = thunderstorm;
         } else if (description === 'snow') {
-
+            icon = snow;
         } else if (description === 'mist') {
-
+            icon = mist;
         }
-
         return icon;
     }
 
