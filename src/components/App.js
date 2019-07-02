7f09b75f485008e8e651ca407db5f42be73bcab4
Łukasz Krawczyk <lukasz.krawczyk@vml.com>
2019-07-02 10:38:20 +0200

    Suspense added to search result

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import Search from './Search';
-import SearchResult from './search/SearchResult';
 import { GlobalStyles } from '../styles/GlobalStyles';
 
-export default () => (
-	<div>
-		<GlobalStyles />
-		<Search />
-		<SearchResult />
-	</div>
-);
+const SearchResult = React.lazy(() => import('./search/SearchResult'));
+
+const App = () => {
+	return (
+		<div>
+			<GlobalStyles />
+			<Search />
+			<Suspense fallback={<div>Loading...</div>}>
+				<SearchResult />
+			</Suspense>
+		</div>
+	);
+}
+
+export default App;
